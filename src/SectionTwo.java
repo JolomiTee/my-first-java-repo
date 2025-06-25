5d9202cc4c19e3b50048501c0690f1de323ac881
Jolomi Taiwo <taiwojolomi15@gmail.com>
2025-06-25 14:20:45 +0100

    - my first exercise of calculating the area of a triangle with user input

--- src/SectionTwo.java
+++ src/SectionTwo.java
@@ -3,29 +3,37 @@ import java.util.Scanner;
 
 public class SectionTwo {
     public static  void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+       Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your name: ");
-        String name  = scanner.nextLine();
+       System.out.println("Enter base of triangle: ");
+       int base = scanner.nextInt();
+       scanner.nextLine();
 
-        System.out.println("Enter your age: ");
-        int age = scanner.nextInt();
+       System.out.println("Enter height of triangle: ");
+       int height = scanner.nextInt();
+       int multiplier = base * height;
 
-        System.out.println("What is your GPA?: ");
-        double gpa = scanner.nextDouble();
-
-        System.out.println("Are you a Student? (true/false");
-        boolean isStudent = scanner.nextBoolean();
-
-        System.out.println("hello " + name);
-        System.out.println("You are " + age + " years old");
-        System.out.println("Your GPA is " + gpa + ", not bad");
-
-        if (isStudent) {
-            System.out.println("You are enrolled as a student");
-        } else {
-            System.out.println("You are not enrolled as a student");
-        }
+       double area = 0.5 * multiplier;
+       System.out.println("The area of the triangle is: " + area);
         scanner.close();
     }
 }
+//  NOTES
+//
+/*
+when accepting an integer first then a sting, sometimes there is new line chavacter like `/n` which is passed
+into the reading buffer, and triggers the submission of an empty string to the
+scanner asking for a string. example:
+    System.out.print("What is your age");
+    int age = scanner.nextInt()
+    scanner.nextLine() // Fix for empty string passing
+
+    System.out.print("What is your favourite color");
+    int color = scanner.nextLine()
+
+    when you input an int, it is read as <value>/n whit `/n` being the character for a new line
+    which is then passed to the next input reader as a value, resulting in an empty output for
+    the color
+
+    to fix, after the `int age = scanner.nextInt()` add a `scanner.nextLine()`
+ */
