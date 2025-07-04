fa603175dc1b2c9683e4a178087f6b43f8a1b20b
Jolomi Taiwo <taiwojolomi15@gmail.com>
2025-07-04 18:33:15 +0100

    - learnt about overloaded methods

--- src/Methods.java
+++ src/Methods.java
@@ -4,40 +4,73 @@ public class Methods {
 //        int age= 22;
 //
 //        happyBirthday(name, age);
-        double result = square(5);
+//        double result = square(5);
 //        System.out.println(result);
 
 //        System.out.println(cube(3));
 //        String fullname = getFullName("Spongebob", "Square pants");
 //        System.out.println(fullname);
 
-        System.out.println(ageCheck(23));
+//        System.out.println(ageCheck(23));
+
+//        System.out.println(add(2,3, 4));
+
+        String pizza = bakePizza("flat bread", "mozarella", "pineapple");
+        System.out.println(pizza);
+
+
     }
 
-    static void happyBirthday (String name, int age) {
-        System.out.println("Happy birthday to you");
-        System.out.printf("Happy birthday to %s\n", name);
-        System.out.printf("You are %d years old\n", age);
-        System.out.println("Happy birthday to you");
+
+
+
+
+//    static void happyBirthday (String name, int age) {
+//        System.out.println("Happy birthday to you");
+//        System.out.printf("Happy birthday to %s\n", name);
+//        System.out.printf("You are %d years old\n", age);
+//        System.out.println("Happy birthday to you");
+//    }
+//
+//    static double square(double number) {
+//        return  number * number;
+//    }
+//
+//    static double cube(double number) {
+//        return number + number + number;
+//    }
+//
+//    static String getFullName(String first, String last){
+//        return first + " " + last;
+//    }
+//
+//    static boolean ageCheck (int age){
+//        if (age >= 18) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+
+//    OVERLOADED FUNCTIONS
+
+//    static double add(double a, double b){
+//        return  a + b;
+//    }
+//
+//    static double add (double a, double b, double c) {
+//        return a + b + c;
+//    }
+
+    static  String bakePizza (String bread) {
+        return bread + " pizza";
     }
 
-    static double square(double number) {
-        return  number * number;
+    static  String bakePizza (String bread, String cheese) {
+        return cheese + " " + bread + " pizza";
     }
 
-    static double cube(double number) {
-        return number + number + number;
-    }
-
-    static String getFullName(String first, String last){
-        return first + " " + last;
-    }
-
-    static boolean ageCheck (int age){
-        if (age >= 18) {
-            return true;
-        } else {
-            return false;
-        }
+    static  String bakePizza (String bread, String cheese, String topping) {
+        return topping + " " + cheese + " " + bread + " pizza";
     }
 }
