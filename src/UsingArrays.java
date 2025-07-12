ab1360b4d0096b3ac8f6d642ca39907dd3578e8e
Jolomi Taiwo <taiwojolomi15@gmail.com>
2025-07-12 12:04:43 +0100

    - learning about arrays in  java - defining array sizes before use - small for loop manipulation with arrays

--- src/UsingArrays.java
+++ src/UsingArrays.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class UsingArrays {
     public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
 //        String[] fruit = {""};
 //        fruit[0] = "pineapple";
 
@@ -23,11 +25,67 @@ public class UsingArrays {
 //            System.out.println(fruits);
 //        }
 
-        String[] foods = {};
-        foods[0] = "pizza";
+        String[] foods = {"apple", "orange", "banana"};
 
-        System.out.println(foods.length);
+//        System.out.println(foods.length);
 
+        // allocate space to the array
+
+//        String[] foods = new String[3];
+//
+//        foods[0] = "eba";
+//        foods[1] = "rice";
+//
+
+//}
+
+//        String[] foods;
+//        int size;
+//
+//        System.out.print("What number of food items do you want? ");
+//        size = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        foods = new String[size];
+//
+//
+//        for (int i = 0; i < foods.length; i++) {
+//            System.out.println("Enter a food: ");
+//            foods[i] = scanner.nextLine();
+//        }
+//
+//        for (String food: foods){
+//            System.out.println(food);
+//        }
+
+//        int[] numbers = {1,2,3,4,5,6};
+//        String target;
+//        boolean isFound = false;
+//
+//        System.out.println("Enter a fruit to find: ");
+//        target = scanner.nextLine();
+
+//        for (int i = 0; i < numbers.length; i++) {
+//            if (target == numbers[i]) {
+//                System.out.println("Element found at index: " + i);
+//                isFound = true;
+//                break;
+//            }
+//        }
+
+//        for (int i = 0; i < foods.length; i++) {
+//            if (foods[i].equals(target)) {
+//                System.out.println("Element found at index: " + i);
+//                isFound = true;
+//                break;
+//            }
+//        }
+//
+//        if (!isFound) {
+//            System.out.println("Element not found in array");
+//        }
+
+        scanner.close();
 
     }
 }
